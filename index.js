584953eef3c2cdbc9e89bfa4a0b6805de798b873
Khoa Pham <khoaphp@yahoo.com>
2016-11-28 09:24:23 +0700

    add sth

--- index.js
+++ index.js
@@ -9,7 +9,75 @@ app.get('/', function(request, response){
   response.render("trangchu");
 });
 
-app.get('/info/:id', function(req, res){
+app.get('/info/:id/:username', function(req, res){
   var i = req.params.id;
-  res.send(i);
+  var u = req.params.username;
+  res.send(i + u);
 });
+
+app.get('/dangnhap', function(req, res){
+
+});
+
+app.get('/chao/:ten', function(req, res){
+  var name = req.params.ten;
+  res.send('Xin chao ban ' + name);
+});
+
+app.get('/tinh/:pt/:a/:b', function(req, res){
+  var pt = req.params.pt;
+  var a = parseInt(req.params.a);
+  var b = parseInt(req.params.b);
+  // var kq;
+  // switch (pt) {
+  //   case 'cong':
+  //     kq = a + b;
+  //     break;
+  //   case 'tru':
+  //     kq = a - b;
+  //     break;
+  //   case 'nhan':
+  //     kq = a * b;
+  //     break;
+  //   case 'chia':
+  //     kq = a / b;
+  //     break;
+  //   default:
+  //     kq = "Khong xac dinh"
+  // }
+  // res.send(`${a} ${pt} ${b} = ${kq}`);
+  var phepTinh = new PhepTinh(pt, a, b);
+  res.send(phepTinh.getMessage());
+});
+
+function PhepTinh(pt, a, b){
+  this.phepTinh = pt;
+  this.soA = a;
+  this.soB = b;
+  var that = this;
+  function getResult(){
+    //Tinh ket qua
+    var kq;
+    switch (that.phepTinh) {
+      case 'cong':
+        kq = that.soA + that.soB;
+        break;
+      case 'tru':
+        kq = that.soA - that.soB;
+        break;
+      case 'nhan':
+        kq = that.soA * that.soB;
+        break;
+      case 'chia':
+        kq = that.soA / that.soB;
+        break;
+      default:
+        kq = "Khong xac dinh"
+    }
+    return kq;
+  }
+  this.getMessage = function(){
+    //Tra ve chuoi
+    return `${this.soA} ${this.phepTinh} ${this.soB} = ${getResult()}`
+  }
+}
